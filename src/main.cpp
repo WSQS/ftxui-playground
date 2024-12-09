7e645b90c7b0d4430b59974b625b528c14425516
WSQSylock <wsqsylock@gmail.com>
2024-12-09 19:39:21 +0800

    add effect of centered text

--- src/main.cpp
+++ src/main.cpp
@@ -16,14 +16,11 @@
 using namespace ftxui;
 
 ButtonOption
-Style()
-{
+Style() {
     auto option = ButtonOption::Animated(Color::Red, Color::Black, Color::Black, Color::White);
-    option.transform = [](const EntryState& s)
-    {
+    option.transform = [](const EntryState &s) {
         auto element = text(s.label);
-        if (s.focused)
-        {
+        if (s.focused) {
             element |= bold;
         }
         return element | center | borderEmpty | flex;
@@ -32,12 +29,10 @@ Style()
 }
 
 int
-main()
-{
+main() {
     using namespace ftxui;
 
-    auto summary = [&]
-    {
+    auto summary = [&] {
         auto content = vbox({
             hbox({text(L"- done:   "), text(L"3") | bold}) | color(Color::Green),
             hbox({text(L"- active: "), text(L"2") | bold}) | color(Color::RedLight),
@@ -51,40 +46,30 @@ main()
 
     MenuOption option = MenuOption::Vertical();
     auto screen = ScreenInteractive::Fullscreen();
-    std::string directoryPath = "/mnt/c";
-    auto func = [&]
-    {
+    std::string directoryPath = "/home/sophomore";
+    auto func = [&] {
         std::filesystem::path directory{directoryPath};
         directory = directory.append(entries[selected]).lexically_normal();
         directoryPath = directory;
-        if (directory.root_directory() == directoryPath)
-        {
+        if (directory.root_directory() == directoryPath) {
             entries.clear();
-        }
-        else
-        {
+        } else {
             entries = {".."};
         }
-        if (is_directory(directory))
-        {
-            for (const auto& entry : std::filesystem::directory_iterator(directory))
-            {
+        if (is_directory(directory)) {
+            for (const auto &entry: std::filesystem::directory_iterator(directory)) {
                 entries.push_back(entry.path().filename());
             }
-        }
-        else
-        {
+        } else {
             auto command = std::string("code ") + directory.string();
             std::filesystem::path temp_directory{directoryPath};
             directory = directory.append("..").lexically_normal();
             directoryPath = directory;
-            for (const auto& entry : std::filesystem::directory_iterator(directory))
-            {
+            for (const auto &entry: std::filesystem::directory_iterator(directory)) {
                 entries.push_back(entry.path().filename());
             }
             std::thread commandThread{
-                [command]()
-                {
+                [command]() {
                     std::system(command.c_str());
                 }
             };
@@ -93,74 +78,79 @@ main()
     };
     option.on_enter = func;
     auto menu = Menu(&entries, &selected, option);
-    for (const auto& entry : std::filesystem::directory_iterator(directoryPath))
-    {
+    for (const auto &entry: std::filesystem::directory_iterator(directoryPath)) {
         entries.push_back(entry.path().filename());
     }
-    auto btn_dec_01 = Button("-1", [&value]
-    {
+    auto btn_dec_01 = Button("-1", [&value] {
         value = rand() % 50;
     }, Style());
-    auto btn_dec_02 = Button("-1", [&value,&entries]
-    {
+    auto btn_dec_02 = Button("-1", [&value,&entries] {
         value = rand() % 50;
         entries.push_back(std::to_string(value));
         // menu->Add(Button("-2",[]{}));
     }, Style());
     // auto btn_dec_02 = Button("+1", [&value] { value += 1; }, Style());
     auto document = //
-        vbox({
-            hbox({
+            vbox({
+                hbox({
+                    summary(),
+                    summary(),
+                    summary() | flex,
+                }),
                 summary(),
                 summary(),
-                summary() | flex,
-            }),
-            summary(),
-            summary(),
-        });
+            });
     // auto buttons = Container::Horizontal ({ btn_dec_02 });
     InputOption input_option{};
     input_option.multiline = false;
-    input_option.on_enter = [&]
-    {
-        if (!std::filesystem::exists(directoryPath))
-        {
+    input_option.on_enter = [&] {
+        if (!std::filesystem::exists(directoryPath)) {
             directoryPath = "/";
         }
         std::filesystem::path directory{directoryPath};
-        if (directory.root_directory() == directoryPath)
-        {
+        if (directory.root_directory() == directoryPath) {
             entries.clear();
-        }
-        else
-        {
+        } else {
             entries = {".."};
         }
-        if (is_regular_file(directory))
-        {
-            for (const auto& entry : std::filesystem::directory_iterator(directory))
-            {
+        if (is_regular_file(directory)) {
+            for (const auto &entry: std::filesystem::directory_iterator(directory)) {
                 entries.push_back(entry.path().filename());
             }
-        }
-        else
-        {
+        } else {
             directoryPath = std::string("code ") + directory.string();
             entries.push_back(directoryPath);
             std::system(directoryPath.c_str());
         }
     };
+    input_option.transform = [](InputState state) {
+        state.element |= color(Color::White);
+
+        if (state.is_placeholder) {
+            state.element |= dim;
+        }
+
+        if (state.focused) {
+            state.element |= inverted;
+        } else {
+            state.element |= center;
+        }
+        if (state.hovered) {
+            state.element |= bgcolor(Color::GrayDark);
+        }
+
+        return state.element;
+    };
     auto input = Input(&directoryPath, input_option);
     auto container = Container::Vertical({input, menu});
-    auto component = Renderer(container, [&]
-    {
+    auto component = Renderer(container, [&] {
         return vbox({
-                input->Render(),
-                separator(),
-                // buttons->Render () | flex,
-                menu->Render() |yframe,
-            }) |
-            flex | border;
+                   input->Render(),
+                   separator(),
+                   // buttons->Render () | flex,
+                   menu->Render() | yframe,
+               }) |
+               flex | border;
     });
     // Limit the size of the document to 80 char.
     // document = document; //| size(WIDTH, LESS_THAN, 80);
