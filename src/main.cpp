91b21b1aae28e99b999a53d2a008933479f96b80
WSQSylock <wsqsylock@gmail.com>
2024-12-13 21:20:09 +0800

    clean up code

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,10 @@
 #include <iostream>
 #include <filesystem>
 #include <ftxui/dom/elements.hpp>
-#include <ftxui/screen/screen.hpp>
-#include <ftxui/screen/string.hpp>
 #include "ftxui/component/screen_interactive.hpp"
 
 #include "ftxui/component/component.hpp"
-#include "ftxui/component/captured_mouse.hpp"  // for ftxui
-#include "ftxui/component/component.hpp"  // for Button, Horizontal, Renderer
 #include "ftxui/component/component_base.hpp"      // for ComponentBase
-#include "ftxui/component/screen_interactive.hpp"  // for ScreenInteractive
-#include "ftxui/dom/elements.hpp"  // for separator, gauge, text, Element, operator|, vbox, border
 
 
 using namespace ftxui;
@@ -31,19 +25,8 @@ Style() {
 int
 main() {
     using namespace ftxui;
-
-    auto summary = [&] {
-        auto content = vbox({
-            hbox({text(L"- done:   "), text(L"3") | bold}) | color(Color::Green),
-            hbox({text(L"- active: "), text(L"2") | bold}) | color(Color::RedLight),
-            hbox({text(L"- queue:  "), text(L"9") | bold}) | color(Color::Red),
-        });
-        return window(text(L" Summary "), content);
-    };
-    auto value = 50;
     std::vector<std::string> entries{".."};
     int selected = 0;
-
     MenuOption option = MenuOption::Vertical();
     auto screen = ScreenInteractive::Fullscreen();
     std::string directoryPath = "/home/sophomore";
@@ -82,26 +65,6 @@ main() {
     for (const auto &entry: std::filesystem::directory_iterator(directoryPath)) {
         entries.push_back(entry.path().filename());
     }
-    auto btn_dec_01 = Button("-1", [&value] {
-        value = rand() % 50;
-    }, Style());
-    auto btn_dec_02 = Button("-1", [&value,&entries] {
-        value = rand() % 50;
-        entries.push_back(std::to_string(value));
-        // menu->Add(Button("-2",[]{}));
-    }, Style());
-    // auto btn_dec_02 = Button("+1", [&value] { value += 1; }, Style());
-    auto document = //
-            vbox({
-                hbox({
-                    summary(),
-                    summary(),
-                    summary() | flex,
-                }),
-                summary(),
-                summary(),
-            });
-    // auto buttons = Container::Horizontal ({ btn_dec_02 });
     InputOption input_option{};
     input_option.multiline = false;
     input_option.on_enter = [&] {
