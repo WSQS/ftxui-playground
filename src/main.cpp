1f12300d0e3da05d2bea089f4e45fceb57275d84
王时青 <sophomore@duck.com>
2024-12-18 15:36:35 +0800

    add tab layer

--- src/main.cpp
+++ src/main.cpp
@@ -7,10 +7,33 @@
 using namespace ftxui;
 using namespace playground;
 
-int main() {
+int main()
+{
     auto screen = ScreenInteractive::Fullscreen();
     std::vector<std::shared_ptr<path_data>> path_datas;
-    auto component = FileMenu(path_datas);
+    Components menus;
+    menus.push_back(FileMenu(path_datas));
+    menus.push_back(FileMenu(path_datas));
+    int select = 0;
+    std::vector<std::string> tab_values{
+        "tab_1",
+        "tab_2",
+        "tab_3",
+    };
+    auto tab_toggle = Toggle(&tab_values, &select);
+    auto tab_container = Container::Tab(menus, &select);
+    auto container = Container::Vertical({
+        tab_toggle,
+        tab_container,
+    });
+    auto renderer = Renderer(container, [&] {
+  return vbox({
+             tab_toggle->Render(),
+             separator(),
+             tab_container->Render(),
+         }) |
+         border;
+});
     // Limit the size of the document to 80 char.
     // document = document; //| size(WIDTH, LESS_THAN, 80);
 
@@ -19,7 +42,7 @@ int main() {
     //
     // std::cout << screen.ToString() << '\0' << std::endl;
 
-    screen.Loop(component);
+    screen.Loop(renderer);
     // auto foldermenu = folder_menu(input_data);
     // screen.Loop(foldermenu);
     return EXIT_SUCCESS;
