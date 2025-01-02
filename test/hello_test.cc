3a6b0c89814849555b9d57d28a54f9a35dcd723c
Sophomore <Sophomore@duck.com>
2025-01-02 15:07:57 +0800

    add filesystem exists tests and reorder include statements

--- test/hello_test.cc
+++ test/hello_test.cc
@@ -1,6 +1,6 @@
 
-#include <gtest/gtest.h>
 #include "filesystem_wrapper.h"
+#include <gtest/gtest.h>
 
 // Demonstrate some basic assertions.
 TEST(HelloTest, BasicAssertions) {
@@ -17,11 +17,15 @@ TEST(filesystem, get_directory_content) {
     EXPECT_EQ(command.size(), stander.size());
     std::set command_set(command.begin(), command.end());
     std::set stander_set(stander.begin(), stander.end());
-    for (auto file:command) {
-        EXPECT_EQ(stander_set.count(file),1);
-    }
-    for (auto file:stander) {
-        EXPECT_EQ(command_set.count(file),1);
-    }
+    for (auto file : command)
+        EXPECT_EQ(stander_set.count(file), 1);
+    for (auto file : stander)
+        EXPECT_EQ(command_set.count(file), 1);
 }
 
+TEST(filesystem, exists) {
+    EXPECT_EQ(filesystem::command::exists("/123"), false);
+    EXPECT_EQ(filesystem::stander::exists("/123"), false);
+    EXPECT_EQ(filesystem::command::exists("/mnt"), false);
+    EXPECT_EQ(filesystem::stander::exists("/mnt"), false);
+}
